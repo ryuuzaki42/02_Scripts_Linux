b2f8ee79e7e64baf5ec4df5eee6aad91c67cf705
João Batista <joao42lbatista@gmail.com>
2023-06-19 16:53:25 -0300

    rm_old_logs_JBs.sh - \\n to \n \\b to \n \\t to \t

--- rm_old_logs_JBs.sh
+++ rm_old_logs_JBs.sh
@@ -24,7 +24,7 @@
 #
 # Folder by default: cd /var/log/removed_scripts/, /var/log/removed_packages/, /var/log/
 #
-# Last update: 16/04/2023
+# Last update: 19/06/2023
 #
 RED='\e[1;31m'
 GREEN='\e[1;32m'
@@ -32,15 +32,15 @@ NC='\033[0m' # reset/no color
 #BLUE='\e[1;34m'
 CYAN='\e[1;36m'
 
-echo -e  "\\n$CYAN # Remove old \"log\" from remove packages #"
-echo -e  " Select the packages logs with the same \"pkg_name\" in folder to be deleted (leave the most recent) $NC\\n"
+echo -e  "\n$CYAN # Remove old \"log\" from remove packages #"
+echo -e  " Select the packages logs with the same \"pkg_name\" in folder to be deleted (leave the most recent) $NC\n"
 
-echo -en "$RED Script in beta - use at your own risk!\\nHit enter to contine or Crtl + C to quit.$NC"
+echo -en "$RED Script in beta - use at your own risk!\nHit enter to contine or Crtl + C to quit.$NC"
 read -r _
 
 func_files_remove() {
     rm_pkg=$1
-    echo -e "\\n${CYAN}List of \"pkg_name\" in the folder $GREEN\"$PWD\"$NC"
+    echo -e "\n${CYAN}List of \"pkg_name\" in the folder $GREEN\"$PWD\"$NC"
 
     list_files=$(ls -1tpL | grep -v '/') # List only files
 
@@ -69,19 +69,19 @@ func_files_remove() {
 
     tmp_file=$(mktemp)
     for file in $list_files; do
-        echo -e "\\n${CYAN}pkg_name: $GREEN$file$NC"
+        echo -e "\n${CYAN}pkg_name: $GREEN$file$NC"
         echo "$list_files_tmp" | grep "^$file"
 
         echo "$list_files_tmp" | grep "^$file" | sed '1d' >> "$tmp_file"
         #read
     done
 
-    echo -e "\\n${CYAN}Folder: \"$GREEN$PWD/\"\\n\\n${RED}Files to be deleted:$NC"
+    echo -e "\n${CYAN}Folder: \"$GREEN$PWD/\"\n\n${RED}Files to be deleted:$NC"
     cat "$tmp_file"
 
     list_files_delete=$(cat "$tmp_file")
     if [ "$list_files_delete" != '' ]; then
-        echo -en "\\n$RED Delete theses files? (y)es or (n)o: "
+        echo -en "\n$RED Delete theses files? (y)es or (n)o: "
         read -r continue_or_not
 
         if [ "$continue_or_not" == 'y' ]; then
@@ -89,10 +89,10 @@ func_files_remove() {
                 rm -v "$file"
             done
         else
-            echo -e "\\n$GREEN # Not deleting files! #$NC"
+            echo -e "\n$GREEN # Not deleting files! #$NC"
         fi
     else
-        echo -e "\\n$GREEN # No file to be deleted! #$NC"
+        echo -e "\n$GREEN # No file to be deleted! #$NC"
     fi
 
     rm "$tmp_file"
