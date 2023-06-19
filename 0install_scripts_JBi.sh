cda60051c8dc8f5d4873f99f2b988dcd71125a71
João Batista <joao42lbatista@gmail.com>
2023-06-19 16:17:01 -0300

    0install_scripts_JBi.sh - \\n to \n \\b to \n \\t to \t

--- 0install_scripts_JBi.sh
+++ 0install_scripts_JBi.sh
@@ -3,15 +3,15 @@
 # Copy the scripts (*_JBs.sh) int this folder to /usr/bin:
 # You can just execute this script
 #
-# Last update: 09/10/2022
+# Last update: 19/06/2023
 #
-echo -e "This script copy (cp *_JBs.sh) to /usr/bin/\\n"
+echo -e "This script copy (cp *_JBs.sh) to /usr/bin/\n"
 
 echo "List of files that will be copied:"
 ls --color=auto ./*_JBs.sh
-echo -e "\\t\\nBe careful, will overwrite the files if they already exists\\n"
+echo -e "\t\nBe careful, will overwrite the files if they already exists\n"
 
-echo -en "Want continue and copy this files?\\n(y)es - (n)o: "
+echo -en "Want continue and copy this files?\n(y)es - (n)o: "
 read -r continueCopy
 
 if [ "$continueCopy" == 'y' ]; then
@@ -19,9 +19,9 @@ if [ "$continueCopy" == 'y' ]; then
 
     if su - root -c "cd $PWD
     cp -v *_JBs.sh /usr/bin/"; then
-        echo -e "\\n\\tThe files was copied"
+        echo -e "\n\tThe files was copied"
     fi
 else
-    echo -e "\\n\\tThe files was not copied"
+    echo -e "\n\tThe files was not copied"
 fi
-echo -e "\\nEnd of the script\\n"
+echo -e "\nEnd of the script\n"
