de15be563dc640a07f95d0b23d353ae119244062
João Batista <joao42lbatista@gmail.com>
2023-06-19 17:03:34 -0300

    0install_configs_JBi.sh - \\n to \n \\b to \n \\t to \t

--- bash_profile/0install_configs_JBi.sh
+++ bash_profile/0install_configs_JBi.sh
@@ -3,27 +3,27 @@
 # Copy the configurations on this folder to ~/ and /root/
 # You can just execute this script
 #
-# Last update: 09/10/2022
+# Last update: 19/06/2023
 #
-echo -e "This script copy (cp .??*) to ~/ and /root/\\n"
+echo -e "This script copy (cp .??*) to ~/ and /root/\n"
 echo "List of files that will be copied:"
 ls .??*
-echo -e "\\t\\nBe careful, will overwrite the files if they already exists\\n"
+echo -e "\t\nBe careful, will overwrite the files if they already exists\n"
 
-echo -en "Want continue and copy this files?\\n(y)es - (n)o: "
+echo -en "Want continue and copy this files?\n(y)es - (n)o: "
 read -r continueCopy
 
 if [ "$continueCopy" == 'y' ]; then
     cp .??* ~/
-    echo -e "\\n\\tThe files was copied to \"$(cd ~ || exit; pwd)/\""
+    echo -e "\n\tThe files was copied to \"$(cd ~ || exit; pwd)/\""
 
-    echo -e "\\nInsert the root password to copy the files to \"/root/\""
+    echo -e "\nInsert the root password to copy the files to \"/root/\""
     if su - root -c "cd $PWD; cp .??* /root/"; then
-        echo -e "\\n\\tThe files was copied to \"/root/\""
+        echo -e "\n\tThe files was copied to \"/root/\""
     else
-        echo -e "\\n\\tThe files was not copied to \"/root/\""
+        echo -e "\n\tThe files was not copied to \"/root/\""
     fi
 else
-    echo -e "\\n\\tThe files was not copied"
+    echo -e "\n\tThe files was not copied"
 fi
-echo -e "\\nEnd of the script\\n"
+echo -e "\nEnd of the script\n"
