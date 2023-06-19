f363789942426a6a158fa4cd36697c699c4084cd
João Batista <joao42lbatista@gmail.com>
2023-06-19 16:56:06 -0300

    wifi_usual_JBs.sh - \\n to \n \\b to \n \\t to \t

--- wifi_usual_JBs.sh
+++ wifi_usual_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script:  usual / common day-to-day functions with Wi-Fi
 #
-# Last update: 29/05/2023
+# Last update: 19/06/2023
 #
 # Tip: If have NetworkManager installed and running, you can use nmtui
 #
@@ -48,7 +48,7 @@ notUseColor() {
 
 colorPrint=$1
 if [ "$colorPrint" == "noColor" ]; then
-    echo -e "\\nColors disabled"
+    echo -e "\nColors disabled"
     shift
 else # Some colors for script output - Make it easier to follow
     useColor
@@ -57,14 +57,14 @@ fi
 
 notPrintHeaderHeader=$1
 if [ "$notPrintHeaderHeader" != "notPrintHeader" ]; then
-    echo -e "$BLUE        #___ Script to usual commands ___#$NC\\n"
+    echo -e "$BLUE        #___ Script to usual commands ___#$NC\n"
 else
     shift
 fi
 
 testColorInput=$1
 if [ "$testColorInput" == "testColor" ]; then
-    echo -e "\\n    Test colors: $RED RED $WHITE WHITE $PINK PINK $BLACK BLACK $BLUE BLUE $GREEN GREEN $CYAN CYAN $NC NC\\n"
+    echo -e "\n    Test colors: $RED RED $WHITE WHITE $PINK PINK $BLACK BLACK $BLUE BLUE $GREEN GREEN $CYAN CYAN $NC NC\n"
     shift
 fi
 
@@ -75,13 +75,13 @@ loadDevWirelessInterface() {
         devInterface="wlan0"
     fi
 
-    echo -e "\\nWorking with the dev interface: $devInterface"
-    echo -e "You can pass other interface as a parameter\\n"
+    echo -e "\nWorking with the dev interface: $devInterface"
+    echo -e "You can pass other interface as a parameter\n"
 }
 
 insertRootPasswordMsg() {
     if [ "$(whoami)" != "root" ]; then
-        echo -e "$CYAN\\nInsert the root password to continue$NC\\n"
+        echo -e "$CYAN\nInsert the root password to continue$NC\n"
     fi
 }
 
@@ -92,10 +92,10 @@ case $optionInput in
 
         loadDevWirelessInterface "$2"
 
-        echo -e "\\n/usr/sbin/iw dev $devInterface link:"
+        echo -e "\n/usr/sbin/iw dev $devInterface link:"
         /usr/sbin/iw dev "$devInterface" link
 
-        echo -e "\\n/sbin/iwconfig $devInterface:"
+        echo -e "\n/sbin/iwconfig $devInterface:"
         /sbin/iwconfig "$devInterface"
         ;;
     "--help" | "-h" | "help" | 'h' | '' | 'w' )
@@ -122,7 +122,7 @@ case $optionInput in
             "--help" | "-h" | "help" | 'h' )
                 help() {
                     echo -e "$CYAN# Show this help message (the same result with \"help\", \"--help\", \"-h\" or 'h') $NC"
-                    echo -e "$CYAN\\nOptions:\\n$RED    Obs$CYAN:$RED * root required,$PINK + NetworkManager required$CYAN\\n"
+                    echo -e "$CYAN\nOptions:\n$RED    Obs$CYAN:$RED * root required,$PINK + NetworkManager required$CYAN\n"
 
                     countOption=0
                     optionVectorSize=${#optionVector[*]}
@@ -137,15 +137,15 @@ case $optionInput in
                 ;;
             '' | 'w' )
                 whiptailMenu() {
-                    echo -e "$CYAN# Menu with whiptail, where you can call the options above (the same result with 'w' or '') #$NC\\n"
+                    echo -e "$CYAN# Menu with whiptail, where you can call the options above (the same result with 'w' or '') #$NC\n"
                     eval "$(resize)"
 
                     heightWhiptail=$((LINES - 5))
                     widthWhiptail=$((COLUMNS - 5))
 
                     if [ "$LINES" -lt 16 ]; then
-                        echo -e "$RED\\nTerminal with very small size. Use one terminal with at least 16 columns (actual size line: $LINES columns: $COLUMNS)$NC"
-                        echo -e "$GREEN\\nRunning: $0 $colorPrint notPrintHeader --help$CYAN\\n" | sed 's/  / /g'
+                        echo -e "$RED\nTerminal with very small size. Use one terminal with at least 16 columns (actual size line: $LINES columns: $COLUMNS)$NC"
+                        echo -e "$GREEN\nRunning: $0 $colorPrint notPrintHeader --help$CYAN\n" | sed 's/  / /g'
                         $0 "$colorPrint" notPrintHeader --help
                     else
                         heightMenuBoxWhiptail=$((LINES - 15))
@@ -167,7 +167,7 @@ case $optionInput in
 
                         if [ "$itemSelected" != '' ]; then
                             itemSelected=${itemSelected// /} # Remove space in the end of selected item
-                            echo -e "$GREEN\\nRunning: $0 $colorPrint notPrintHeader $itemSelected ${*} $CYAN\\n" | sed 's/  / /g'
+                            echo -e "$GREEN\nRunning: $0 $colorPrint notPrintHeader $itemSelected ${*} $CYAN\n" | sed 's/  / /g'
                             $0 "$colorPrint" notPrintHeader "$itemSelected" "${*}"
                         fi
                     fi
@@ -178,7 +178,7 @@ case $optionInput in
         esac
         ;;
     "cr" )
-        echo -e "$CYAN# Create configuration to connect to Wi-Fi network (in /etc/wpa_supplicant.conf) #$NC\\n"
+        echo -e "$CYAN# Create configuration to connect to Wi-Fi network (in /etc/wpa_supplicant.conf) #$NC\n"
 
         loadDevWirelessInterface "$2"
         echo "Visible networks:"
@@ -187,21 +187,21 @@ case $optionInput in
         createWifiConfig() {
             continueOrNot=0
             while [ "$continueOrNot" == 0 ]; do
-                echo -en "\\nName of the network (SSID): "
+                echo -en "\nName of the network (SSID): "
                 read -r netSSID
 
-                echo -en "\\nPassword of this network: "
+                echo -en "\nPassword of this network: "
                 read -r -s netPassword
 
                 if [ "$(wc -c <<< "$netPassword")" -lt 8 ]; then
-                    echo -e "\\n\\nError: Passphrase must be 8..63 characters\\n Insert the values again!"
+                    echo -e "\n\nError: Passphrase must be 8..63 characters\n Insert the values again!"
                 else
                     continueOrNot=1
                 fi
             done
 
             wpa_passphrase "$netSSID" "$netPassword" | grep -v "#psk" >> /etc/wpa_supplicant.conf
-            echo -e "\\nConfiguration of the network \"$netSSID\" saved\\n"
+            echo -e "\nConfiguration of the network \"$netSSID\" saved\n"
         }
         export -f createWifiConfig
 
@@ -210,26 +210,26 @@ case $optionInput in
         su root -c 'createWifiConfig' # In this case without the hyphen (su - root -c 'command') to no change the environment variables
         ;;
     "cn" )
-        echo -e "$CYAN# Connect to Wi-Fi network (in /etc/wpa_supplicant.conf) #$NC\\n"
+        echo -e "$CYAN# Connect to Wi-Fi network (in /etc/wpa_supplicant.conf) #$NC\n"
 
         loadDevWirelessInterface "$2"
 
         networkConfigAvailable=$(grep "ssid" < /etc/wpa_supplicant.conf)
         if [ "$networkConfigAvailable" == '' ]; then
-            echo -e "$RED\\nError: Not fond any configuration of Wi-Fi networks (in /etc/wpa_supplicant.conf).\\n Try: $0 cr$NC"
+            echo -e "$RED\nError: Not fond any configuration of Wi-Fi networks (in /etc/wpa_supplicant.conf).\n Try: $0 cr$NC"
         else
             connectWifiConfig() {
                 if pgrep -f "NetworkManager" > /dev/null; then # Test if NetworkManager is running
-                    echo -en "\\nKilling NetworkManager..."
+                    echo -en "\nKilling NetworkManager..."
                     killall NetworkManager # kill the previous wpa_supplicant "configuration"
                 fi
 
                 if pgrep -f "wpa_supplicant" > /dev/null; then # Test if wpa_supplicant is running
-                    echo -en "\\nKilling wpa_supplicant..."
+                    echo -en "\nKilling wpa_supplicant..."
                     killall wpa_supplicant # kill the previous wpa_supplicant "configuration"
                 fi
 
-                echo -e "\\nChoose one network to connect\\n"
+                echo -e "\nChoose one network to connect\n"
                 TmpFile=$(mktemp) # Create a tmp file
                 netNumber=1
 
@@ -240,7 +240,7 @@ case $optionInput in
                 done
 
                 cat "$TmpFile"
-                echo -en "\\nNetwork number: "
+                echo -en "\nNetwork number: "
                 read -r networkNumber
                 networkName=$(grep "$networkNumber ssid" < "$TmpFile")
                 networkName=$(sed 's/^.* s/s/g' <<< "$networkName")
@@ -250,27 +250,27 @@ case $optionInput in
                 wpaConf=$(sed -n '/network/!b;:a;/}/!{$!{N;ba}};{/'"$networkName"'/p}' /etc/wpa_supplicant.conf)
 
                 if [ "$wpaConf" == '' ]; then
-                    echo -e "$RED\\nError: Not find configuration to network '$networkName' (in /etc/wpa_supplicant.conf).\\n Try: $scriptName cr$NC"
+                    echo -e "$RED\nError: Not find configuration to network '$networkName' (in /etc/wpa_supplicant.conf).\n Try: $scriptName cr$NC"
                 else
                     TmpFile=$(mktemp)
                     grep -v -E "{|}|ssid|psk" < /etc/wpa_supplicant.conf > "$TmpFile"
 
                     echo -e "$wpaConf" >> "$TmpFile" # Save the configuration of the network on this file
 
-                    echo -e "\\n########### Network configuration ####################"
+                    echo -e "\n########### Network configuration ####################"
                     cat "$TmpFile"
-                    echo -e "######################################################\\n"
+                    echo -e "######################################################\n"
 
-                    echo -e "\\nConnecting to $networkName by the device $devInterface\\n"
+                    echo -e "\nConnecting to $networkName by the device $devInterface\n"
                     #wpa_supplicant -i wlan0 -c /etc/wpa_supplicant.conf -d -B wext # Normal command
                     wpa_supplicant -i "$devInterface" -c "$TmpFile" -d -B wext # Connect with the network using the tmp file
 
                     rm "$TmpFile"
 
-                    echo -e "\\nGetting IP\\n"
+                    echo -e "\nGetting IP\n"
                     dhclient "$devInterface" # Get IP
 
-                    echo -e "\\nNetwork status\\n"
+                    echo -e "\nNetwork status\n"
                     iw dev "$devInterface" link # Show connection status
                 fi
             }
@@ -286,7 +286,7 @@ case $optionInput in
         fi
         ;;
     "dc" )
-        echo -e "$CYAN# Disconnect of one Wi-Fi network #$NC\\n"
+        echo -e "$CYAN# Disconnect of one Wi-Fi network #$NC\n"
 
         loadDevWirelessInterface "$2"
 
@@ -297,7 +297,7 @@ case $optionInput in
         iw dev $devInterface link"
         ;;
     "l-iw" )
-        echo -e "$CYAN# List the Wi-Fi AP around, with iw (show WPS and more infos) #$NC\\n"
+        echo -e "$CYAN# List the Wi-Fi AP around, with iw (show WPS and more infos) #$NC\n"
 
         loadDevWirelessInterface "$2"
 
@@ -306,7 +306,7 @@ case $optionInput in
         su - root -c "/usr/sbin/iw dev $devInterface scan | grep -E '$devInterface|SSID|signal|WPA|WEP|WPS|Authentication|WPA2|: channel'"
         ;;
     "l-iwlist" )
-        echo -e "$CYAN# List the Wi-Fi AP around, with iwlist (show WPA/2 and more infos) #$NC\\n"
+        echo -e "$CYAN# List the Wi-Fi AP around, with iwlist (show WPA/2 and more infos) #$NC\n"
 
         loadDevWirelessInterface "$2"
 
@@ -315,17 +315,17 @@ case $optionInput in
         /sbin/iwlist "$devInterface" scan | grep -E "Address|ESSID|Frequency|Signal|WPA|WPA2|Encryption|Mode|PSK|Authentication"
         ;;
     "nm-list" )
-        echo -e "$CYAN# List the Wi-Fi AP around with the nmcli from NetworkManager #$NC\\n"
+        echo -e "$CYAN# List the Wi-Fi AP around with the nmcli from NetworkManager #$NC\n"
         nmcli device wifi list
         ;;
     * )
-        echo -e "\\n    $(basename "$0") - Error: Option \"$1\" not recognized"
+        echo -e "\n    $(basename "$0") - Error: Option \"$1\" not recognized"
         echo -e "    Try: $0 '--help'"
         ;;
 esac
 
 if [ "$notPrintHeaderHeader" != "notPrintHeader" ]; then
-    echo -e "$BLUE\\n        #___ So Long, and Thanks for All the Fish ___#$NC"
+    echo -e "$BLUE\n        #___ So Long, and Thanks for All the Fish ___#$NC"
 else
     shift
 fi
