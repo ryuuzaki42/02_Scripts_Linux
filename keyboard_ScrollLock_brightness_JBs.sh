2c668270ab4fccec294a4a0d9ad1dcae11bd7d01
João Batista <joao42lbatista@gmail.com>
2025-07-25 10:57:24 -0300

    Added keyboard_ScrollLock_brightness_JBs.sh and keyboard_ScrollLock_brightness.desktop

---
+++ keyboard_ScrollLock_brightness_JBs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: On keybord with brightness is activate on scroll lock, enable an disable in turn
+#
+# Tip: Add a shortcut to this script
+#
+# Last update: 25/07/2025
+#
+
+value_brightness=$(cat /sys/class/leds/input0::scrolllock/brightness)
+if [ "$value_brightness" == 0 ]; then
+    xset led named "Scroll Lock"
+else
+    xset -led named "Scroll Lock"
+fi
