82e61eb721ac2d9bad9e8cb45a66866894e06437
João Batista <joao42lbatista@gmail.com>
2026-02-13 13:33:47 -0300

    run_prog_localy_JBs.sh - Added

---
+++ run_prog_localy_JBs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Run a program using $HOME in working directory ($PWD)
+#
+# Last update: 13/02/2026
+#
+set -x
+
+# Here
+HERE=$(pwd)
+echo -e "\nLocal place: $HERE\n"
+
+# Folder to use as $HOME (~/)
+mkdir 1_home/ || exit 1
+
+# Folder to use as $XDG_CONFIG_HOME (~/.config/)
+mkdir 2_config/ || exit 1
+
+# Program name
+prog_name="$1"
+
+# Run the program using the new configurations folders
+HOME="$HERE/1_home/" XDG_CONFIG_HOME="$HERE/2_config/" ./$prog_name
