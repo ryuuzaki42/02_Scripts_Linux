d535b82ef81b8076523262bb1dcc9367cf9c5af5
João Batista <joao42lbatista@gmail.com>
2026-07-08 15:18:58 -0300

    Update 0_install_scripts_JBi.sh

--- 0_install_scripts_JBi.sh
+++ 0_install_scripts_JBi.sh
@@ -4,13 +4,15 @@
 #
 # Last update: 08/07/2026
 #
-echo -e "This script copy (cp *_JBs.sh) to /usr/bin/\n"
 
-echo "List of files that will be copied:"
 RED='\e[1;31m'
 GREEN='\e[1;32m'
 NC='\033[0m' # reset/no color
 BLUE='\e[1;34m'
+
+echo -e "\n${BLUE}This script copy/install (cp *_JBs.sh) to /usr/bin/$NC\n"
+
+echo "List of files/scripts that will be copied:"
 ls --color=auto ./*_JBs.sh
 echo -e "\t\n${RED}Be careful, will overwrite the files if they already exists$NC\n"
 
