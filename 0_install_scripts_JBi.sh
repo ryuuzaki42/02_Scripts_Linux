cb79197d474bf7d4f3c7523d27c3388f0ff24feb
João Batista <joao42lbatista@gmail.com>
2026-07-09 07:18:57 -0300

    Update 0_install_scripts_JBi.sh

--- 0_install_scripts_JBi.sh
+++ 0_install_scripts_JBi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Copy/Install the scripts (*_JBs.sh) in this folder to /usr/bin/:
+# Copy/Install the scripts (*_JBs.sh) in this folder to /usr/bin/
 #
-# Last update: 08/07/2026
+# Last update: 09/07/2026
 #
 
 RED='\e[1;31m'
@@ -14,9 +14,9 @@ echo -e "\n${BLUE}This script copy/install (cp *_JBs.sh) to /usr/bin/$NC\n"
 
 echo "List of files/scripts that will be copied:"
 ls --color=auto ./*_JBs.sh
-echo -e "\t\n${RED}Be careful, will overwrite the files if they already exists$NC\n"
+echo -e "\t\n${RED}Be careful Will overwrite the files if they already exists$NC\n"
 
-echo -en "Want continue and copy this files?\n(y)es - (n)o: "
+echo -en "Want continue and copy these files?\n(y)es - (n)o: "
 read -r continueCopy
 
 if [ "$continueCopy" == 'y' ]; then
@@ -24,9 +24,8 @@ if [ "$continueCopy" == 'y' ]; then
 
     if su - root -c "cd $PWD
     cp -v *_JBs.sh /usr/bin/"; then
-        echo -e "\n\t${GREEN}The files was copied$NC"
+        echo -e "\n${GREEN}The files was copied$NC\n"
     fi
 else
-    echo -e "\n\t${RED}The files was not copied$NC"
+    echo -e "\n${RED}The files was not copied$NC\n"
 fi
-echo -e "\nEnd of the script\n"
