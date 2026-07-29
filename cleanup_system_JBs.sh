386c31c6eb68fac6dc409594d574ce055cb0c067
João Batista <joao42lbatista@gmail.com>
2026-07-28 21:35:09 -0300

    Update cleanup_system_JBs.sh

--- cleanup_system_JBs.sh
+++ cleanup_system_JBs.sh
@@ -70,6 +70,7 @@ if [ "$continue_or_not_1" != 'y' ] && [ "$continue_or_not_1" != '' ]; then
     echo -e "\nJust exiting\n"
     exit 0
 fi
+echo
 
 if echo "$home_folder" | grep -q '/$'; then # Check if last character is '/'
     home_folder=${home_folder::-1} # Remove the last character - '/'
