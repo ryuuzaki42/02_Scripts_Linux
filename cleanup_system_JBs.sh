95a6202a6484d93937e64af9f431c4b172b0aec8
João Batista <joao42lbatista@gmail.com>
2026-07-28 21:18:07 -0300

    Update cleanup_system_JBs.sh - In 'test' mode use print, in normal mode delete with rm -rvf

--- cleanup_system_JBs.sh
+++ cleanup_system_JBs.sh
@@ -54,7 +54,7 @@ echo -e "- user_name: $user_name\n- home_user: $home_user\n"
 
 if [ "$only_test" != '' ]; then
     echo -e "# Test mode - files and folders will not be deleted #\n"
-    delete_file=() # Empty array
+    delete_file=(-print) # Only print files/folders name
 else
     delete_file=(-exec rm -rvf -- {} +) # Delete files/folders
 fi
@@ -195,7 +195,7 @@ done
 
 # Delete all files/folders empty in /tmp/
 echo -e "\n    # Removing all empty files in /tmp/ not recursively"
-find /tmp/ -maxdepth 1 -empty -print "${delete_file[@]}" # -delete
+find /tmp/ -maxdepth 1 -empty "${delete_file[@]}" # -delete
 
 if [ "$clean_all" == "all" ]; then # Delete .ICE-unix .X11-unix plasma-csd-generator.* sddm-auth*
     echo -en "\nDelete empty files/folders in /tmp/ folder. Continue? (y)es or (n)o: "
@@ -215,8 +215,8 @@ if [ "$clean_all" == "all" ]; then # Delete .ICE-unix .X11-unix plasma-csd-gener
 
         # Delete all empty (zero size) folders and files in /tmp/ recursively
             # '-name ".mount_*" -prune -o' to ignore all AppImage in use, with mount in "/tmp/.mount_*"
-        find /tmp/ \( "${preserve_X[@]}" \) -prune -o -name ".mount_*" -prune -o -empty -print "${delete_file[@]}" # -delete # Safer to remove empty files
-        find /tmp/ \( "${preserve_X[@]}" \) -prune -o -name ".mount_*" -prune -o -size 0b -print "${delete_file[@]}" # -delete # Remove files with 0b. Depend of block size
+        find /tmp/ \( "${preserve_X[@]}" \) -prune -o -name ".mount_*" -prune -o -empty "${delete_file[@]}" # -delete # Safer to remove empty files
+        find /tmp/ \( "${preserve_X[@]}" \) -prune -o -name ".mount_*" -prune -o -size 0b "${delete_file[@]}" # -delete # Remove files with 0b. Depend of block size
 
         echo -e "\n # Recommendation: Restart your system! #"
     else
