4a69c35566d8b7081c4bbf855a0b4a9ca8aa720a
João Batista <joao42lbatista@gmail.com>
2026-09-22 11:35:32 -0300

    dl_pkgs.org_JBs.sh, editor_test_open_JBs.sh, focus_time_JBs.sh, and 2 more files - update license

--- libreoffice_change_language_config_JBs.sh
+++ libreoffice_change_language_config_JBs.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
+#
 # Autor= João Batista Ribeiro
 # Bugs, Agradecimentos, Críticas "construtivas"
 # me envie um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
-# Este programa é um software livre; você pode redistribui-lo e/ou
-# modifica-lo dentro dos termos da Licença Pública Geral GNU como
-# publicada pela Fundação do Software Livre (FSF); na versão 2 da
-# Licença, ou (na sua opinião) qualquer versão.
+# Este programa é um software livre: você pode redistribuí-lo e/ou
+# modificá-lo sob os termos da Licença Pública Geral GNU (GPL)
+# conforme publicada pela Free Software Foundation, tanto a versão 3
+# da licença, como (a seu critério) qualquer versão posterior.
 #
-# Este programa é distribuído na esperança que possa ser útil,
-# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
-# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+# Este programa é distribuído na esperança de que seja útil,
+# mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO.
+# Consulte a Licença Pública Geral do GNU para mais detalhes.
 #
-# Veja a Licença Pública Geral GNU para mais detalhes.
-# Você deve ter recebido uma cópia da Licença Pública Geral GNU
-# junto com este programa, se não, escreva para a Fundação do Software
-#
-# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-#
-# Descrição: Script to download the last version of Slackware Live, made by AlienBob
+# Descrição: Script to change the language configuration en-US to pt-BR or vice versa
 #
 # Last update: 19/06/2023
 #
